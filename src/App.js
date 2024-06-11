5e70c69e95e0671b90e56fc50702b5782c88e585
MoonKnight777 <moonkinght4505@gmail.com>
2024-06-12 00:23:17 +0530

    hvjgvhj

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/MoonKnight777.github.io/InstaMovies2/' element={<Home/>}/>
         </Routes>
       </div>
     </Router>
