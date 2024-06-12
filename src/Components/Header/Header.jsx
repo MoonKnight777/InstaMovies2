fd925d113f2a7de2f73c4458c2d68402f3fd1e2c
MoonKnight777 <moonkinght4505@gmail.com>
2024-06-12 17:45:39 +0530

    lst_try

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import { IoMdSearch } from "react-icons/io";
 import "./Header.scss"
-
+import logo from"./letter-i.png"
 export const Header = () => {
     return (
         <nav className='header'>
             <div className="left">
-                <img src='/images/letter-i.png' alt="I" />
+                <img src={logo} alt="I" />
                 <span className='logo-sta'>NSTA</span>
                 <span className='logo-movies'>MOVIES</span>
             </div>
